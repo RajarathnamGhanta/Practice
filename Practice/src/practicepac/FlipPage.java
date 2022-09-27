868ba8219357d5750c9716c6aa01408d54c86888
Dell <Dell@DESKTOP-HVG97IH>
2022-09-27 16:28:23 +0530

    just now i have won

---
+++ Practice/src/practicepac/FlipPage.java
@@ -0,0 +1,15 @@
+package practicepac;
+
+public class FlipPage {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=13;
+		int c;
+		
+			c=a+b;
+			System.out.println(c);
+
+	}
+
+}
